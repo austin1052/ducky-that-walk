f8615d52f46d867ef33608065392ba8024e99c81
austin1052 <austin1052@gmail.com>
2023-01-17 17:40:02 -0500

    add loading screen

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { ref, onValue } from "firebase/database";
-import { db } from './config/index.js';
-import AdminDashboard from './routes/AdminDashboard.js';
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { ref, onValue } from "firebase/database"
+import { db } from './config/index.js'
+import AdminDashboard from './routes/AdminDashboard.js'
 import Scores from "./routes/Scores.js"
-import Home from './routes/Home.js';
-import CreatePlayer from './routes/CreatePlayer.js';
-import { MobileContextProvider } from './context/MobileContext.js';
-import './styles/globals.css';
+import LoadingScreen from './components/LoadingScreen.js'
+import CreatePlayer from './routes/CreatePlayer.js'
+import { MobileContextProvider } from './context/MobileContext.js'
+import './styles/globals.css'
 
 
 function App() {
   const [allQueensData, setAllQueensData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    const queensRef = ref(db, "queens/");
+    const queensRef = ref(db, "queens/")
     onValue(queensRef, (snapshot) => {
       if (snapshot.exists()) {
-        const data = snapshot.val();
-        setAllQueensData(data);
+        const data = snapshot.val()
+        setAllQueensData(data)
       }
-    });
+    })
+    setIsLoading(false)
   }, [])
 
   return (
     <MobileContextProvider>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/scores" element={<Scores />} />
-          <Route path="/create-player" element={<CreatePlayer allQueensData={allQueensData} />} />
-          <Route path="/admin/scores" element={<AdminDashboard allQueensData={allQueensData} />} />
-        </Routes>
+        {
+          isLoading ?
+            <LoadingScreen /> :
+            <Routes>
+              {/* <Route path="/" element={<Home />} /> */}
+              <Route path="/scores" element={<Scores />} />
+              <Route path="/create-player" element={<CreatePlayer allQueensData={allQueensData} />} />
+              <Route path="/admin/scores" element={<AdminDashboard allQueensData={allQueensData} />} />
+            </Routes>
+        }
       </div>
     </MobileContextProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,13 +1,11 @@
-import { Link } from "react-router-dom"
 import logo from "../assets/ducky-that-walk-logo.png"
 import styles from "../styles/Home.module.css"
 
-export default function Home() {
+export default function LoadingScreen() {
   return (
     <div className={styles.container}>
       <img src={logo} className={styles.logo} alt="ducky that walk" />
       <div className={styles.duck}>🦆</div>
-      <Link to="/scores" className={styles.link}>View Scores</Link>
     </div>
   )
 }
